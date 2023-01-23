391ab1f67feaacd9d028891d8498fe54ce46355f
Piotr Walczak <piotrandrzejwalczak@gmail.com>
2023-01-23 15:25:12 +0100

    Add products counter

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,9 @@ shopping_list = {
 }
 shopping_list["piekarnia"] = [f.capitalize() for f in shopping_list["piekarnia"]]
 shopping_list["warzywniak"] = [f.capitalize() for f in shopping_list["warzywniak"]]
+
+count = 0
+for key, value in shopping_list.items():
+    if isinstance(value, list):
+         count += len(value)
+
